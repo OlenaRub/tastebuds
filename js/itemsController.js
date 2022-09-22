ea300a6d8bd39bc106a53a57dd5d9f0fff9d40ef
gjoebeck <gjoebeck@gmail.com>
2022-09-22 13:40:18 -0700

    Modified itemsController.js -Galen

---
+++ js/itemsController.js
@@ -0,0 +1,9 @@
+class recipesController {
+  constructor(items) {
+    this.items = [];
+    //currentId(0)
+    // names ingredients instructions images
+  }
+}
+const recipe = new recipesController('Recipe');
+console.log(recipe.items);
