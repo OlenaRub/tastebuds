7d8e371826333f58fe31d5470d78ef98d72c1643
Olena <olena@Benjamins-Air-2.attlocal.net>
2022-09-23 11:18:45 -0400

    Olena Task5

--- js/index.js
+++ js/index.js
@@ -1,38 +1,10 @@
 // Initialize a new TaskManager with currentId set to 0
 const itemsController = new ItemsController(0);
-
-// Select the New Task Form
-const newItemForm = document.querySelector('#newItemForm');
-
-// Add an 'onsubmit' event listener
-newItemForm.addEventListener('submit', (event) => {
-    // Prevent default action
-    event.preventDefault();
-    // Select the inputs
-    const newItemNameInput = document.querySelector('#newItemNameInput');
-    const newItemDescription = document.querySelector('#newItemDescription');
-
-    /*
-        Validation code here
-    */
-
-    // Get the values of the inputs
-    const name = newItemNameInput.value;
-    const description = newItemDescription.value;
-    const createdAt = new Date();
-
-    // Add the task to the task manager
-    itemsController.addItem(name, description, createdAt);
-
-    // Clear the form
-    newItemNameInput.value = '';
-    newItemDescription.value = '';
-});
-
+alert("Task5");
 
 function addItemCard(item){
-    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+    const itemHTML = '<div class="card" style="width: 20rem;">\n' +
+        '    <img src="'+item.img +'" width="300" height="250"  alt="product image">\n' +
         '    <div class="card-body">\n' +
         '        <h5 class="card-title">'+item.name+'</h5>\n' +
         '        <p class="card-text">'+item.description+'</p>\n' +
@@ -44,10 +16,24 @@ function addItemCard(item){
     itemsContainer.innerHTML += itemHTML;
 }
 
-addItemCard({'name':'juice',
-    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-    'description':'Orange and Apple juice fresh and delicious'});
+function loadStorageSampleData(){
+    if(!localStorage.getItem("items")){
+        const sampleItems = [{'name':'Borsch',
+        'img':'images/borsch.jpeg',
+        'description':'Ukranian Borsch'},
+        {'name':'Small House',
+        'img':'images/smallhouse.jpeg',
+        'description':'Ukranian Cake'}];
+        localStorage.setItem("items", JSON.stringify(sampleItems));
+    }
+}
 
-addItemCard({'name':'Tayto',
-    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
-    'description':'Cheese & Onion Chips'})
+function loadCardsListFromItemsController(){
+    for(var i = 0, size = itemsController.items.length; i < size ; i++){
+        const item = itemsController.items[i];
+        addItemCard(item);
+    }
+}
+loadStorageSampleData();
+itemsController.loadItemsFromLocalStorage();
+loadCardsListFromItemsController();
--- js/itemsController.js
+++ js/itemsController.js
@@ -1,9 +1,34 @@
-class recipesController {
-  constructor(items) {
-    this.items = [];
-    //currentId(0)
-    // names ingredients instructions images
+
+// Create a ItemsController class
+class ItemsController {
+  // Set up the items and currentId property in the contructor
+  constructor(currentId = 0) {
+      this.items = [];
+      this.currentId = currentId;
+  }
+   
+  // Create the addItem method
+  addItem(name, description,imageUrl) {
+      const item = {
+          // Increment the currentId property
+          id: this.currentId++,
+          name: name,
+          description: description,
+          imageUrl: imageUrl
+      };
+
+      // Push the item to the items property
+      this.items.push(item);
+  }   
+
+  loadItemsFromLocalStorage() {
+      const storageItems = localStorage.getItem("items")
+      if (storageItems) {
+          const items = JSON.parse(storageItems)
+          for (var i = 0, size = items.length; i < size; i++) {
+              const item = items[i];
+              this.items.push(item);
+          }
+      }
   }
 }
-const recipe = new recipesController('Recipe');
-console.log(recipe.items);
